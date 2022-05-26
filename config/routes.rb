fbecb8d5f484a35476800cb3aba3714339197cbe
zsh <rayane-09-38@live.fr>
2022-05-26 16:40:57 +0200

    adding landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'superpowers#index'
+  root to: 'superpowers#landing'
 
   resources :superpowers do
     resources :bookings, only: [:new, :create]
